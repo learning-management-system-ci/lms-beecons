a67879e60fe348a8cfa6b8b3392eb010d22a2b92
Fakhri112 <dracozen112@gmail.com>
2022-09-01 14:36:37 +0700

    layout fix

--- app/Views/layouts/authentication_layout.php
+++ app/Views/layouts/authentication_layout.php
@@ -20,15 +20,7 @@
         </section>
         <section style="border: 2px solid rgba(0, 0, 0, 0);" class="d-flex">
             <div class="form-wrap">
-                <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
-                    integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
-                <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-                    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
-                    crossorigin="anonymous">
-                </script>
                 <?= $this->renderSection('authentication-component') ?>
-                <?= $this->renderSection('authentication-js')?>
             </div>
             <div class="logo">
                 <h3>LOGO</h3>
