a26588e52999cef7f6ae1bcc60faf29be85144f3
salunaalavi <salunaalavi@gmail.com>
2022-09-01 11:22:17 +0700

    moduling views

---
+++ app/Views/pages/authentication/login.php
@@ -0,0 +1,24 @@
+<?=$this->extend('layouts/authentication_layout')?>
+
+<?=$this->section('authentication-component')?>
+<form action="" class=" form d-flex flex-column" style="border: 2px solid rgba(236, 236, 236, 0);">
+    <p class="welcome-text">Welcome</p>
+    <p class="sign-in-text">Sign Up</p>
+    <p class="info-text">Please input your email and password</p>
+    <label for="user_email" class="form-label">Email</label>
+    <input type="text" name="email" id="user_email" placeholder="Email">
+    <label for="user_pass" class="form-label mt-3">Password</label>
+    <input type="password" name="pass" id="user_pass" placeholder="Password">
+    <div class="option d-flex justify-content-between align-items-center">
+        <div class="checkbox d-flex align-items-center">
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+            <label for="vehicle1">Remember Me</label><br>
+        </div>
+        <a href="#">Forgot Password?</a>
+    </div>
+    <button type="button" class="btn btn-primary">Sign in</button>
+    <p class="sign-up" style="text-align: center;">Don't Have Account <a href="#">Sign up</a></p>
+    <p class="horizontal">Or</p>
+    <button class="google"><img src="image/google.png" alt=""></button>
+</form>
+<?=$this->endSection()?>
