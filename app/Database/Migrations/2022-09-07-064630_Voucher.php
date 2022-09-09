3a6c09dfc076541cb513eb1c3cd1adcf89fc3f3e
burhanusr <bsultanramadan@gmail.com>
2022-09-09 15:51:08 +0700

    add seeders

--- app/Database/Migrations/2022-09-07-064630_Voucher.php
+++ app/Database/Migrations/2022-09-07-064630_Voucher.php
@@ -6,33 +6,38 @@ use CodeIgniter\Database\Migration;
 
 class Voucher extends Migration
 {
-    public function up() {
+    public function up()
+    {
         $this->forge->addField([
-			'voucher_id'          => [
-				'type'           => 'INT',
-				'constraint'     => 5,
-				'unsigned'       => true,
-				'auto_increment' => true
-			],
-			'title'      => [
-				'type'           => 'VARCHAR',
-				'constraint'     => 100,
-			],
-			'description'      => [
-				'type'           => 'VARCHAR',
-				'constraint'     => 255,
-			],
+            'voucher_id'          => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'title'      => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 100,
+            ],
+            'description'      => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 255,
+            ],
+            'is_active'      => [
+                'type'           => 'BOOLEAN',
+                'default'           => FALSE,
+            ],
             'code'      => [
                 'type'           => 'VARCHAR',
-				'constraint'     => 20,
-			],
+                'constraint'     => 20,
+            ],
             'discount_price'      => [
                 'type'           => 'INT',
                 'constraint'     => 20,
             ],
             'created_at datetime default current_timestamp',
             'updated_at datetime default current_timestamp on update current_timestamp',
-		]);
+        ]);
 
         $this->forge->addKey('voucher_id', TRUE);
         $this->forge->createTable('voucher', TRUE);
