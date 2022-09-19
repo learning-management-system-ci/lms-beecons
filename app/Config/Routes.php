5a1f4eee7b72fac1e4d23a6a244971e9821b10d3
rikzan fernanda <fernandarikzan@gmail.com>
2022-09-19 09:04:22 +0700

    change route for homepage

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,7 +37,7 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'AuthController::indexLogin');
+$routes->get('/', 'Home::index');
 $routes->get('/login', 'Api\AuthController::indexLogin');
 
 $routes->get('/login/loginWithGoogle', 'AuthController::loginWithGoogle');
