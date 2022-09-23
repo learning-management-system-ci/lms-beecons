9b5054524cd5975ad5131f8cc7c43df6ab4508e8
Luthfi A Pratama <luthfianump@gmail.com>
2022-09-23 14:54:50 +0700

    add MainSeeder

--- app/Database/Seeds/MainSeeder.php
+++ app/Database/Seeds/MainSeeder.php
@@ -8,7 +8,12 @@ class MainSeeder extends Seeder
 {
     public function run()
     {
+        $this->call('CategorySeeder');
+        $this->call('CourseCategorySeeder');
+        $this->call('CourseSeeder');
         $this->call('FaqSeeder');
+        $this->call('JobSeeder');
+        $this->call('PapSeeder');
         $this->call('VoucherSeeder');
     }
 }
