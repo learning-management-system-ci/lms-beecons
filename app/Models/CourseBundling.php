6b78cbe7b1f6b13c9fab4ef3b7b10b85860e0750
vandinta <89644669+vandinta@users.noreply.github.com>
2022-09-23 14:28:46 +0700

    update

--- app/Models/CourseBundling.php
+++ app/Models/CourseBundling.php
@@ -22,28 +22,6 @@ class CourseBundling extends Model
         $query = $builder->get();
         return $query->getResultArray();
 	}
-    
-    // function getBundlingId(){
-    //     $builder = $this->db->table('course_bundling');
-    //     // $builder->select('bundling_id');
-    //     $builder->countAllResults();
-    //     $query = $builder->get();
-    //     return $query->getResult();
-	// }
-
-    function getBundlingId(){
-        $builder = $this->db->table('course_bundling');
-        // // $builder->select('bundling_id');
-        // $builder->countAllResults();
-        $query = $builder->get();
-        $hasil = $query->getResultArray();
-
-        // foreach ($hasil as $row) {
-        //     echo $row->bundling_id; // access attributes
-        //     // echo $user->reverseName(); // or methods defined on the 'User' class
-        // }
-        return $hasil;
-	}
 
     function getDataBundling($data_bundling_id){
         $builder = $this->db->table('bundling');
@@ -53,13 +31,6 @@ class CourseBundling extends Model
         return $query->getResultArray();
 	}
 
-    function getCourseId(){
-        $builder = $this->db->table('course');
-        $builder->select('course_id');
-        $query = $builder->get();
-        return $query->getRow();
-	}
-
     function getDataCourse($data_course_id){
         $builder = $this->db->table('course');
         $builder->select('*');
@@ -67,82 +38,6 @@ class CourseBundling extends Model
         $query = $builder->get();
         return $query->getResultArray();
 	}
-
-    function getCourse(){
-        // $this->db->table('users')->getWhere(['email'=>$email])->getRowArray();
-		return $this->db->query('SELECT course_id FROM course_bundling')->getResult();
-        // $row = $query->getRowArray();
-        // return $this->db->table('course_bundling')
-        // ->join('course','course.course_id=course_bundling.course_id', 'inner')
-        // ->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id', 'inner')
-        // ->get()->getResultArray();
-        // $builder = $this->db->table('course_bundling');
-        // $builder->join('course','course.course_id=course_bundling.course_id', 'inner');
-        // $builder->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id', 'inner');
-        // $query = $builder->get();
-        // return $query->getResultArray();
-        // $query = $builder->getResult();
-        // if($query->num_rows() != 0) {
-        //     return $query->result_array();
-        // }else{
-        //     return false;
-        // }
-	}
-
-    // function getCourse(){
-    //     // $this->db->table('users')->getWhere(['email'=>$email])->getRowArray();
-	// 	// return $this->db->query('SELECT course_id FROM course_bundling')->getResult();
-    //     // $row = $query->getRowArray();
-    //     // return $this->db->table('course_bundling')
-    //     // ->join('course','course.course_id=course_bundling.course_id', 'inner')
-    //     // ->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id', 'inner')
-    //     // ->get()->getResultArray();
-    //     $builder = $this->db->table('course_bundling');
-    //     $builder->join('course','course.course_id=course_bundling.course_id', 'inner');
-    //     $builder->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id', 'inner');
-    //     $query = $builder->get();
-    //     return $query->getResultArray();
-    //     // $query = $builder->getResult();
-    //     // if($query->num_rows() != 0) {
-    //     //     return $query->result_array();
-    //     // }else{
-    //     //     return false;
-    //     // }
-	// }
-
-    // function getBundling(){
-    //     // $this->db->table('users')->getWhere(['email'=>$email])->getRowArray();
-	// 	// return $this->db->query('SELECT bundling_id FROM course_bundling')->getResult();
-    //     $builder = $this->db->table('course_bundling');
-    //     $builder->select('course.title as title_course, course.description as description_course, course.price as price_course, course.thumbnail, bundling.title as title_bundling, bundling.description as description_bundling, bundling.price as price_bundling');
-    //     $builder->join('course','course.course_id=course_bundling.course_id');
-    //     $builder->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id');
-    //     $query = $builder->get();
-    //     return $query->getResultArray();
-    //     // $row = $query->getRowArray();
-    //     // return $this->db->table('course_bundling')
-    //     // ->join('course','course.course_id=course_bundling.course_id', 'inner')
-    //     // ->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id', 'inner')
-    //     // ->get()->getResultArray();
-	// }
-
-    // function getData(){
-    //     $builder = $this->db->table('course_bundling');
-    //     $builder->select('course.title as title_course, course.description as description_course, course.price as price_course, course.thumbnail, bundling.title as title_bundling, bundling.description as description_bundling, bundling.price as price_bundling');
-    //     $builder->join('course','course.course_id=course_bundling.course_id');
-    //     $builder->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id');
-    //     $query = $builder->get();
-    //     return $query->getResultArray();
-	// }
-
-    // function getData(){
-    //     $builder = $this->db->table('course_bundling');
-    //     $builder->select('course.title as title_course, course.description as description_course, course.price as price_course, course.thumbnail');
-    //     $builder->join('course','course.course_id=course_bundling.course_id');
-    //     // $builder->join('bundling', 'bundling.bundling_id=course_bundling.bundling_id');
-    //     $query = $builder->get();
-    //     return $query->getResultArray();
-	// }
 
     function getShow($id){
         $builder = $this->db->table('course_bundling');
