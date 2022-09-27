a5bb271c80d34f2df1ca9e14897aaf8e141bbb76
vandinta <89644669+vandinta@users.noreply.github.com>
2022-09-27 21:30:09 +0700

    update user video

--- app/Database/Seeds/VideoSeeder.php
+++ app/Database/Seeds/VideoSeeder.php
@@ -8,6 +8,44 @@ class VideoSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $data = [
+            [
+                'course_id' => 4,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+            [
+                'course_id' => 1,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+            [
+                'course_id' => 3,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+            [
+                'course_id' => 2,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+            [
+                'course_id' => 1,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+            [
+                'course_id' => 4,
+                'title' => 'Lorem Ipsum is simply dummy',
+                'video' => 'Video 1',
+                'order' => 2,
+            ],
+        ];
+        $this->db->table('video')->insertBatch($data);
     }
 }
--- app/Models/UserVideo.php
+++ app/Models/UserVideo.php
@@ -7,36 +7,67 @@ use CodeIgniter\Model;
 class UserVideo extends Model
 {
     protected $DBGroup          = 'default';
-    protected $table            = 'uservideos';
-    protected $primaryKey       = 'id';
+    protected $table            = 'user_video';
+    protected $primaryKey       = 'user_video_id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['video_id', 'user_id', 'score', 'created_at', 'updated_at'];
 
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    // function getUserVideo(){
+    //     $builder = $this->db->table('user_video');
+    //     $builder->select('*');
+    //     $query = $builder->get();
+    //     return $query->getResultArray();
+	// }
 
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
+    function getDataVideo($data_video_id){
+        $builder = $this->db->table('video');
+        $builder->select('*');
+        $builder->where('video_id', $data_video_id);
+        $query = $builder->get();
+        return $query->getResultArray();
+	}
 
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    // function getDataCourse($data_course_id){
+    //     $builder = $this->db->table('course');
+    //     $builder->select('*');
+    //     $builder->where('course_id', $data_course_id);
+    //     $query = $builder->get();
+    //     return $query->getResultArray();
+	// }
+
+    function getDataUser($data_user_id){
+        $builder = $this->db->table('users');
+        $builder->select('*');
+        $builder->where('id', $data_user_id);
+        $query = $builder->get();
+        return $query->getResultArray();
+	}
+
+    function getShow($id){
+        $builder = $this->db->table('user_video');
+        // $builder->select('*');
+        $builder->where('user_video_id', $id);
+        $query = $builder->get();
+        return $query->getResultArray();
+	}
+
+    // function getShowCourse($course_id){
+    //     $builder = $this->db->table('course');
+    //     $builder->select('*');
+    //     $builder->where('course_id', $course_id);
+    //     $query = $builder->get();
+    //     return $query->getResultArray();
+	// }
+
+    function getShowUser($user_id){
+        $builder = $this->db->table('user_video');
+        $builder->select('*');
+        $builder->where('user_id', $user_id);
+        $query = $builder->get();
+        return $query->getResultArray();
+	}
 }
