11bc46025ac2bf4ace13db71016669255cdc77e5
salunaalavi <salunaalavi@gmail.com>
2022-10-04 08:36:24 +0700

    Slicing page referral code

--- app/Views/layouts/app_layout.php
+++ app/Views/layouts/app_layout.php
@@ -80,6 +80,8 @@
 
     <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+
     <!-- bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
