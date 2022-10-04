7849d6c8f63b7b91b8489994e00f8bae001298d4
Hendrikus Riko <hendrykus48@gmail.com>
2022-10-04 10:34:41 +0700

    fix route

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -6,8 +6,21 @@ use CodeIgniter\Model;
 
 class Cart extends Model
 {
-    protected $table = 'cart';
-    protected $primaryKey = 'cart_id';
-    protected $DBGroup = 'default';
-    protected $allowedFields = ['user_id', 'course_id', 'bundling_id', 'total'];
+    protected $DBGroup          = 'default';
+    protected $table            = 'cart';
+    protected $primaryKey       = 'cart_id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['user_id', 'course_id', 'bundling_id', 'total'];
+
+    function getCart($userId) {
+        $builder = $this->db->table('cart');
+                    $builder->select('*');
+                    $builder->where('user_id', $userId);
+                    $query = $builder->get();
+                    return $query;
+    }
 }
