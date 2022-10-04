b3c12ae11685b0ab610d9cc063da8f12c8931871
salunaalavi <salunaalavi@gmail.com>
2022-10-04 12:00:37 +0700

    Revisi theme

--- app/Views/pages/authentication/otp_code.php
+++ app/Views/pages/authentication/otp_code.php
@@ -3,14 +3,14 @@
 <?=$this->section('authentication-component')?>
 <form action="<?= base_url('/api/send-otp'); ?>" id="otp-code" class=" form d-flex flex-column"
     style="border: 2px solid rgba(236, 236, 236, 0); width: 290px;">
-    <p class="welcome-text">Check your email!</p>
+    <p class="welcome-text">Cek email kamu!</p>
     <p class="sign-in-text"><?= $title; ?></p>
-    <p class="info-text">Please input OTP code that sent to you</p>
+    <p class="info-text">Silahkan masukkan kode OTP yang telah kami kirimkan</p>
     <label for="otp" class="form-label">OTP</label>
-    <input type="text" name="otp" id="otp" placeholder="Enter your OTP code">
-    <button class="btn btn-primary mt-3" id="button" type="submit" disabled="disabled"
+    <input type="text" name="otp" id="otp" placeholder="Masukkan kode OTP">
+    <button class="btn mt-3" id="button" type="submit" disabled="disabled"
         style="border: 0;">Confirm</button>
-    <p class="sign-up" style="text-align: center;">Remember Your Account? <a href="<?= base_url('login'); ?>"
+    <p class="sign-up" style="text-align: center;">Ingat akun kamu? <a href="<?= base_url('login'); ?>"
             style="text-decoration: none;">Sign
             in</a></p>
 </form>
