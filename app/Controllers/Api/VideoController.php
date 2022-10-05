a3e1de4fa319a34150c09050b5e84d330ecb5983
Wahyu Triono <70625779+whytrno@users.noreply.github.com>
2022-10-05 12:53:03 +0700

    Update VideoController.php

--- app/Controllers/Api/VideoController.php
+++ app/Controllers/Api/VideoController.php
@@ -6,6 +6,7 @@ use App\Models\Video;
 use App\Models\Course;
 use App\Models\Users;
 use App\Models\Quiz;
+use App\Models\VideoCategory;
 use CodeIgniter\RESTful\ResourceController;
 use Firebase\JWT\JWT;
 
@@ -18,6 +19,7 @@ class VideoController extends ResourceController {
 	function __construct(){
 		$this->videoModel = new Video();
 		$this->courseModel = new Course();
+		$this->videoCategory = new VideoCategory();
 	}
 
 	public function index($id = null) {
@@ -37,70 +39,70 @@ class VideoController extends ResourceController {
 
   public function create() {
 		$key = getenv('TOKEN_SECRET');
-  	$header = $this->request->getServer('HTTP_AUTHORIZATION');
-	  if (!$header) return $this->failUnauthorized('Akses token diperlukan');
-	  $token = explode(' ', $header)[1];
+		$header = $this->request->getServer('HTTP_AUTHORIZATION');
+		if (!$header) return $this->failUnauthorized('Akses token diperlukan');
+		$token = explode(' ', $header)[1];
 
-    try {
-			$decoded = JWT::decode($token, $key, ['HS256']);
-			$user = new Users;
+		try {
+				$decoded = JWT::decode($token, $key, ['HS256']);
+				$user = new Users;
 
-    	// cek role user
-	    $data = $user->select('role')->where('id', $decoded->uid)->first();
-	    if($data['role'] != 'admin'){
-				return $this->fail('Tidak dapat di akses selain admin', 400);
-	    }
-
-			$rules = [
-				"video_category_id" => "required",
-				"title" => "required",
-				"video" => "uploaded[video]|mime_in[video,video/mp4,video/3gp,video/flv]|max_size[video,262144]",
-				"order" => "required",
-			];
-			$messages = [
-				"video_category_id" => [
-					"required" => "{field} tidak boleh kosong"
-				],
-				"title" => [
-					"required" => "{field} tidak boleh kosong"
-				],
-				"video" => [
-					'uploaded' => '{field} tidak boleh kosong',
-					'mime_in' => 'File Extention Harus Berupa mp4, 3gp, atau flv',
-					'max_size' => 'Ukuran File Maksimal 2 MB'
-				],
-				"order" => [
-					"required" => "{field} tidak boleh kosong"
-				],
-			];
-			if (!$this->validate($rules, $messages)) return $this->fail($this->validator->getErrors());
-			
-			$verifyCourse = $this->courseModel->where("video_category_id", $this->request->getVar('video_category_id'))->first();
-			if(!$verifyCourse) {
-				return $this->failNotFound('Course tidak ditemukan');
-			} else {
-				$dataVideo = $this->request->getFile('video');
-				$fileName = $dataVideo->getRandomName();
-				$this->videoModel->insert([
-					'video_category_id' => $this->request->getVar("video_category_id"),
-					'title' => $this->request->getVar("title"),
-					'order' => $this->request->getVar("order"),
-					'video' => $fileName
-				]);
-				$dataVideo->move('upload/course-video/', $fileName);
-	
-				$response = [
-					'status' => 200,
-					'success' => 200,
-					'message' => 'Video berhasil diupload',
-					'data' => []
-				];
+			// cek role user
+			$data = $user->select('role')->where('id', $decoded->uid)->first();
+			if($data['role'] != 'admin'){
+					return $this->fail('Tidak dapat di akses selain admin', 400);
 			}
-			return $this->respondCreated($response);
-		} catch (\Throwable $th) {
-    	return $this->fail('Akses token tidak sesuai');
-    }
-  }
+
+				$rules = [
+					"video_category_id" => "required",
+					"title" => "required",
+					"video" => "uploaded[video]|mime_in[video,video/mp4,video/3gp,video/flv]|max_size[video,262144]",
+					"order" => "required",
+				];
+				$messages = [
+					"video_category_id" => [
+						"required" => "{field} tidak boleh kosong"
+					],
+					"title" => [
+						"required" => "{field} tidak boleh kosong"
+					],
+					"video" => [
+						'uploaded' => '{field} tidak boleh kosong',
+						'mime_in' => 'File Extention Harus Berupa mp4, 3gp, atau flv',
+						'max_size' => 'Ukuran File Maksimal 2 MB'
+					],
+					"order" => [
+						"required" => "{field} tidak boleh kosong"
+					],
+				];
+				if (!$this->validate($rules, $messages)) return $this->fail($this->validator->getErrors());
+				
+				$verifyCourse = $this->videoCategory->where("video_category_id", $this->request->getVar('video_category_id'))->first();
+				if(!$verifyCourse) {
+					return $this->failNotFound('Course tidak ditemukan');
+				} else {
+					$dataVideo = $this->request->getFile('video');
+					$fileName = $dataVideo->getRandomName();
+					$this->videoModel->insert([
+						'video_category_id' => $this->request->getVar("video_category_id"),
+						'title' => $this->request->getVar("title"),
+						'order' => $this->request->getVar("order"),
+						'video' => $fileName
+					]);
+					$dataVideo->move('upload/course-video/', $fileName);
+		
+					$response = [
+						'status' => 200,
+						'success' => 200,
+						'message' => 'Video berhasil diupload',
+						'data' => []
+					];
+				}
+				return $this->respondCreated($response);
+			} catch (\Throwable $th) {
+			return $this->fail($th->getMessage());
+		}
+	}
 
 	public function update($id = null) {
 		$key = getenv('TOKEN_SECRET');
@@ -180,7 +182,7 @@ class VideoController extends ResourceController {
 			}
 			return $this->respond($response);
 		} catch (\Throwable $th) {
-      return $this->fail('Akses token tidak sesuai');
+      return $this->fail($th->getMessage());
     }
 	}
 
@@ -217,7 +219,7 @@ class VideoController extends ResourceController {
 				return $this->failNotFound('Data Video tidak ditemukan');
 			}
 		} catch (\Throwable $th) {
-      return $this->fail('Akses token tidak sesuai');
+      return $this->fail($th->getMessage());
     }
   }
 }
