94c7cb0bc2111434c82c15ede9a36e115375a93d
rikzan fernanda <fernandarikzan@gmail.com>
2022-10-12 11:36:12 +0700

    update cart page style

--- public/js/home/cart.js
+++ public/js/home/cart.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
-
-    //informasi apabila tidak ada items di keranjang
-    const empty_cart = () => {
-        if ($("table tr").length == 1 || $("table tr").length == 246) {
-
-            $("table")
-                .after('<div class="empty-cart-info d-flex justify-content-center align-items-center">' +
-                    '<h6> Keranjang kamu kosong.Pilih course terbaik kami</h6>' +
-                    '</div>')
-        }
-    }
-
     empty_cart()
 
+    //aksi tombol redeem
+    $("#cart-form-redeem").on("submit", function (event) {
+        event.preventDefault();
+        Swal.fire({
+            // width: '300px',
+            title: "<div class='redeem-loading'> " +
+                '<img class="loading-icon" src="image/cart/redeem-loading.gif" alt=""> ' +
+                '<h4>Sedang diproses</h4> ' +
+                // '<p>Mohon tunggu selagi diproses</p> ' +
+                "</div>",
+            text: 'Mohon tunggu sebentar.',
+            // padding: '0px 0px 40px 6px',
+            showConfirmButton: false,
+            willClose: redeem_success,
+            showClass: {
+                popup: 'animate__animated animate__fadeIn animate__fast'
+            },
+        })
+    })
+
     //popup ketika voucher berhasil diredeem
-    const redeem_success = () => {
+    function redeem_success() {
         Swal.fire({
             title: "<div class='redeem-success'> " +
                 '<img class="mb-4 success-icon" src="image/cart/success-popup.png" alt=""> ' +
-                '<h5 class="mt-4">Berhasil Ditambahkan<h5> ' +
-                '<p>Voucher anda sudah ditambahkan di keranjang</p> ' +
+                '<h5 class="mt-4">Berhasil digunakan<h5> ' +
+                // '<p>Voucher anda sudah ditambahkan di keranjang</p> ' +
                 "</div>",
+            text: 'Kode voucher sudah berhasil digunakan.',
             focusConfirm: false,
-            padding: '0px 0px 42px 50px',
+            // padding: '0px 0px 42px 50px',
             showConfirmButton: false,
             showCloseButton: true,
         })
@@ -51,22 +60,14 @@ $(document).ready(function () {
         empty_cart()
     })
 
-    //aksi tombol redeem
-    $("#redeem").on("click", function (event) {
-        event.preventDefault();
-        Swal.fire({
-            width: '300px',
-            title: "<div class='redeem-loading'> " +
-                '<img class="loading-icon" src="image/cart/redeem-loading.gif" alt=""> ' +
-                '<h5>Wait a sec</h5> ' +
-                '<p>Mohon tunggu selagi diproses</p> ' +
-                "</div>",
-            padding: '0px 0px 40px 6px',
-            showConfirmButton: false,
-            willClose: redeem_success,
-            showClass: {
-                popup: 'animate__animated animate__fadeIn animate__fast'
-            },
-        })
-    })
+    //informasi apabila tidak ada items di keranjang
+    function empty_cart() {
+        if ($("table tr").length == 1 || $("table tr").length == 246) {
+
+            $("table")
+                .after('<div class="empty-cart-info d-flex justify-content-center align-items-center">' +
+                    '<h6> Keranjang kamu kosong.Pilih course terbaik kami</h6>' +
+                    '</div>')
+        }
+    }
 })
