1ae237ae2034d650858f2fec417ad4c790ca5dec
salunaalavi <salunaalavi@gmail.com>
2022-10-20 15:25:33 +0700

    Change post to ajax

--- public/js/authentication/api/login.js
+++ public/js/authentication/api/login.js
@@ -8,47 +8,55 @@ $("#login").submit(function (event) {
 
     // Get some values from elements on the page:
     var $form = $(this),
-        csrf_test_name_passed = $form.find("input[name='csrf_test_name']").val(),
         email_passed = $form.find("input[name='email']").val(),
         password_passed = $form.find("input[name='password']").val(),
         url = $form.attr("action");
 
     $('#loading-modal').modal('toggle');
 
-    // Send the data using post
-    var posting = $.post(url, { csrf_test_name: csrf_test_name_passed, email: email_passed, password: password_passed });
-
-    posting.done(function (data) {
-        res = $(data)
-        Cookies.set('access_token', res[0].data[0], { expires: 1 / 24 });
-        $('#login').unbind("submit");
-        var error_message = data.message;
-        var error = data.status;
-        if (error != null) {
-            $('#loading-modal').modal('hide');
-            $('document').ready(function () {
-                $('.modal-header').addClass("bg-success");
-                $('.modal-title').html("Berhasil");
-                $('#message').html("Tunggu kami mengarahkan anda ke halaman profile...");
-                $('#message-modal').modal('toggle');
-            })
-        };
-        if (error !== 500) {
-            setTimeout(function () {
-                window.location.reload();
-            }, 2000)
-        }
+    $.ajax({
+        url: url,
+        type: "post",
+        data: {
+            email: email_passed,
+            password: password_passed
+        },
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Accept": "aplication/json",
+        },
+        dataType: "json",
+        success: function (data) {
+            res = $(data)
+            Cookies.set('access_token', res[0].data[0], { expires: 1 / 24 });
+            $('#login').unbind("submit");
+            var error = data.status;
+            if (error != null) {
+                $('#loading-modal').modal('hide');
+                $('document').ready(function () {
+                    $('.modal-header').addClass("bg-success");
+                    $('.modal-title').html("Berhasil");
+                    $('#message').html("Tunggu kami mengarahkan anda ke halaman profile...");
+                    $('#message-modal').modal('toggle');
+                })
+            };
+            if (error !== 500) {
+                setTimeout(function () {
+                    window.location.reload();
+                }, 2000)
+            }
+        },
+        error: function (status, error) {
+            var error_message = status.responseJSON.messages.error;
+            if (error_message != null) {
+                $('#loading-modal').modal('hide');
+                $(document).ready(function () {
+                    $('.modal-header').addClass("bg-danger");
+                    $('.modal-title').html("Gagal");
+                    $('#message').html(error_message);
+                    $('#message-modal').modal('toggle');
+                })
+            }
+        },
     });
-    posting.fail(function (status, error) {
-        var error_message = status.responseJSON.messages.error;
-        if (error_message != null) {
-            $('#loading-modal').modal('hide');
-            $(document).ready(function () {
-                $('.modal-header').addClass("bg-danger");
-                $('.modal-title').html("Gagal");
-                $('#message').html(error_message);
-                $('#message-modal').modal('toggle');
-            })
-        }
-    })
 });
