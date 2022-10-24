d5841b8f81cfc456b38d8fbe88f16784ea825ff8
rikzan fernanda <fernandarikzan@gmail.com>
2022-10-24 09:19:39 +0700

    update logo and footer

--- app/Views/components/home/footer.php
+++ app/Views/components/home/footer.php
@@ -1,21 +1,34 @@
 <footer class="footer-container">
     <div class="item">
-        <img src="../../../image/logo.png" alt="logo">
+        <a href="/">
+            <img src="/image/logo.svg" class="app-logo" alt="logo">
+        </a>
+        <div class="mt-5">
+            <p>Download the app by clicking the link below :</p>
+            <div class="footer-app-download row m-0">
+                <div class="col-6 ps-0">
+                    <img src="/image/home/google-play.png" alt="app">
+                </div>
+                <div class="col-6">
+                    <img src="/image/home/app-store.png" alt="app">
+                </div>
+            </div>
+        </div>
     </div>
     <div class="item">
         <h2>Pages</h2>
 
-        <a href="" class="footer-link">Home</a>
-        <a href="" class="footer-link">Pricing</a>
-        <a href="" class="footer-link">Blog</a>
-        <a href="" class="footer-link">Demo</a>
+        <a href="/" class="footer-link">Home</a>
+        <a href="/courses" class="footer-link">Courses</a>
+        <a href="/webinar" class="footer-link">Webinar</a>
+        <a href="/faq" class="footer-link">FAQ</a>
+        <a href="/about-us" class="footer-link">About Us</a>
     </div>
     <div class="item">
         <h2>Service</h2>
 
-        <a href="" class="footer-link">Konstruksi</a>
-        <a href="" class="footer-link">Coding</a>
-        <a href="" class="footer-link">UI/UX Design</a>
+        <a href="" class="footer-link">Bootcamp</a>
+        <a href="/webinar" class="footer-link">Webinar</a>
     </div>
     <div class="item">
         <h2>Contact</h2>
