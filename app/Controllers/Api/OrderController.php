d030139cf66a89e9db706784204eaadf6f242fc1
Hendrikus Riko <hendrykus48@gmail.com>
2022-10-26 08:56:52 +0700

    fix email body otp & activation user

--- app/Controllers/Api/OrderController.php
+++ app/Controllers/Api/OrderController.php
@@ -188,65 +188,106 @@ class OrderController extends BaseController
     }
 
     public function notifHandler() {
-        // Set your Merchant Server Key
-        \Midtrans\Config::$serverKey = 'SB-Mid-server-F7J9pzrwMAM5Af2mTxYpD-kx';
-        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-        \Midtrans\Config::$isProduction = false;
-        // Set sanitization on (default)
-        \Midtrans\Config::$isSanitized = true;
-        // Set 3DS transaction for credit card to true
-        \Midtrans\Config::$is3ds = true;
+        // // Set your Merchant Server Key
+        // \Midtrans\Config::$serverKey = 'SB-Mid-server-F7J9pzrwMAM5Af2mTxYpD-kx';
+        // // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        // \Midtrans\Config::$isProduction = false;
+        // // Set sanitization on (default)
+        // \Midtrans\Config::$isSanitized = true;
+        // // Set 3DS transaction for credit card to true
+        // \Midtrans\Config::$is3ds = true;
 
-        $notif = new \Midtrans\Notification();
-        //var_dump($notif);
-        $notif = $notif->getResponse();
-        $transaction = $notif->transaction_status;
-        $fraud = $notif->fraud_status;
+        // $notif = new \Midtrans\Notification();
+        // //var_dump($notif);
+        // $notif = $notif->getResponse();
+        // $transaction = $notif->transaction_status;
+        // $fraud = $notif->fraud_status;
 
-        error_log("Order ID $notif->order_id: "."transaction status = $transaction, fraud status = $fraud");
+        // error_log("Order ID $notif->order_id: "."transaction status = $transaction, fraud status = $fraud");
 
-        if ($transaction == 'capture') {
-            // For credit card transaction, we need to check whether transaction is challenge by FDS or not
-            if ($type == 'credit_card') {
-                if ($fraud == 'challenge') {
-                    // TODO set payment status in merchant's database to 'Challenge by FDS'
-                    // TODO merchant should decide whether this transaction is authorized or not in MAP
-                    echo "Transaction order_id: " . $notif->order_id ." is challenged by FDS";
-                } else {
-                    // TODO set payment status in merchant's database to 'Success'
-                    echo "Transaction order_id: " . $notif->order_id ." successfully captured using " . $notif->payment_type;
-                }
-            }
-        } else if ($transaction == 'settlement') {
-            $status = 'paid';
-            // TODO set payment status in merchant's database to 'Settlement'
-            echo "Transaction order_id: " . $notif->order_id ." successfully transfered using " . $notif->payment_type;
-        } else if ($transaction == 'pending') {
-            $status = 'pending';
-            // TODO set payment status in merchant's database to 'Pending'
-            echo "Waiting customer to finish transaction order_id: " . $notif->order_id . " using " . $notif->payment_type;
-        } else if ($transaction == 'deny') {
-            $status = 'cancel';
-            // TODO set payment status in merchant's database to 'Denied'
-            echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is denied.";
-        } else if ($transaction == 'expire') {
-            $status = 'cancel';
-            // TODO set payment status in merchant's database to 'expire'
-            echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is expired.";
-        } else if ($transaction == 'cancel') {
-            $status = 'cancel';
-            // TODO set payment status in merchant's database to 'Denied'
-            echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is canceled.";
-        }
+        // if ($transaction == 'capture') {
+        //     // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+        //     if ($type == 'credit_card') {
+        //         if ($fraud == 'challenge') {
+        //             // TODO set payment status in merchant's database to 'Challenge by FDS'
+        //             // TODO merchant should decide whether this transaction is authorized or not in MAP
+        //             echo "Transaction order_id: " . $notif->order_id ." is challenged by FDS";
+        //         } else {
+        //             // TODO set payment status in merchant's database to 'Success'
+        //             echo "Transaction order_id: " . $notif->order_id ." successfully captured using " . $notif->payment_type;
+        //         }
+        //     }
+        // } else if ($transaction == 'settlement') {
+        //     $status = 'paid';
+        //     // TODO set payment status in merchant's database to 'Settlement'
+        //     echo "Transaction order_id: " . $notif->order_id ." successfully transfered using " . $notif->payment_type;
+        // } else if ($transaction == 'pending') {
+        //     $status = 'pending';
+        //     // TODO set payment status in merchant's database to 'Pending'
+        //     echo "Waiting customer to finish transaction order_id: " . $notif->order_id . " using " . $notif->payment_type;
+        // } else if ($transaction == 'deny') {
+        //     $status = 'cancel';
+        //     // TODO set payment status in merchant's database to 'Denied'
+        //     echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is denied.";
+        // } else if ($transaction == 'expire') {
+        //     $status = 'cancel';
+        //     // TODO set payment status in merchant's database to 'expire'
+        //     echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is expired.";
+        // } else if ($transaction == 'cancel') {
+        //     $status = 'cancel';
+        //     // TODO set payment status in merchant's database to 'Denied'
+        //     echo "Payment using " . $type . " for transaction order_id: " . $notif->order_id . " is canceled.";
+        // }
         
 
         $order = new Order;
         $id = $this->request->getVar("order_id");
-		$data = [
-			"order_id" => $this->request->getVar("order_id"),
-			"transaction_status" => $status,
-            "transaction_id" => $this->request->getVar("transaction_id"),
-	    ];
-        $order->update($id, $data);
-    }		
+		// $data = [
+		// 	"order_id" => $this->request->getVar("order_id"),
+		// 	"transaction_status" => $status,
+        //     "transaction_id" => $this->request->getVar("transaction_id"),
+	    // ];
+        // $order->update($id, $data);
+
+        // send receipt payment
+        // $getEmail = $order
+        //     ->select('users.email')
+        //     ->where('order.order_id', $id)
+        //     ->join('users', 'order.user_id=users.id')
+        //     ->findAll();
+        
+        $getOrderData = $order
+            ->select('*')
+            ->where('order.order_id', 1484076229)
+            ->join('order_course', 'order.order_id=order_course.order_id')
+            ->findAll();
+        
+        $data = [
+            "order_id" => $getOrderData[0]["order_id"],
+            "total_price" => $getOrderData[0]["gross_amount"]
+        ];
+        
+        //var_dump($getOrderData);
+        //return 0;
+       // $subject = 'Pembelian Selesai';
+        //$getEmail = $getEmail[0]["email"];
+        return view('/html_email/payment_success.html', $data);
+        
+        //$message = view('/html_email/otp_request.html', $data);
+        $email = \Config\Services::email();
+        $email->setTo($getEmail);
+        $email->setFrom('hendrikusozzie@gmail.com', 'OTP Reset Password');
+        $message = 'sddsddd';
+        $email->setSubject($subject);
+        $email->setMessage($message);
+          
+        $email->send();
+    }
+    
+    public function send() {
+                // send receipt payment
+                $order = new Order;
+                $user = new Users;
+               
+    }
 }
