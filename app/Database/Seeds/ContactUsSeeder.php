dbdb7dee9863c4f05585dd0e733bc7bbc637547f
hendrikusriko <88796037+hendrikusriko@users.noreply.github.com>
2022-11-08 21:43:15 +0700

    fix error contactus seeder

--- app/Database/Seeds/ContactUsSeeder.php
+++ app/Database/Seeds/ContactUsSeeder.php
@@ -12,32 +12,32 @@ class ContactUsSeeder extends Seeder
             [
                 'email' => 'testtt01@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => 'default.png',
+                'question_image' => 'default.png',
             ],
             [
                 'email' => 'testtt02@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => ' ',
+                'question_image' => ' ',
             ],
             [
                 'email' => 'testtt03@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => 'default.png',
+                'question_image' => 'default.png',
             ],
             [
                 'email' => 'testtt04@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => 'default.png',
+                'question_image' => 'default.png',
             ],
             [
                 'email' => 'testtt05@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => ' ',
+                'question_image' => ' ',
             ],
             [
                 'email' => 'testtt06@gmail.com',
                 'question'    => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                'question_picture' => ' ',
+                'question_image' => ' ',
             ],
         ];
         $this->db->table('contact_us')->insertBatch($data);
