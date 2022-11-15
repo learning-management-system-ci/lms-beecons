ceddc4e773319656db04f4c3ca9975757549c1d8
Wahyu Triono <whytrno@gmail.com>
2022-11-15 10:04:04 +0700

    Fix video category and video seeder

--- app/Database/Seeds/VideoCategoryNew.php
+++ app/Database/Seeds/VideoCategoryNew.php
@@ -14,27 +14,25 @@ class VideoCategoryNew extends Seeder
                 'title' => ''
             ],
             [
-                'course_id' => '2',
-                'title' => ''
-            ],
-            [
-                'course_id' => '3',
-                'title' => ''
-            ],
-            [
-                'course_id' => '4',
-                'title' => ''
-            ],
-            [
-                'course_id' => '5',
-                'title' => ''
-            ],
-            [
-                'course_id' => '6',
-                'title' => ''
-            ],
-            [
                 'course_id' => '7',
+                'title' => ''
+            ],
+            [
+                'course_id' => '13',
+                'title' => ''
+            ],
+            [
+                'course_id' => '19',
+                'title' => ''
+            ],
+            [
+                'course_id' => '25',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '2',
                 'title' => ''
             ],
             [
@@ -42,7 +40,43 @@ class VideoCategoryNew extends Seeder
                 'title' => ''
             ],
             [
+                'course_id' => '14',
+                'title' => ''
+            ],
+            [
+                'course_id' => '20',
+                'title' => ''
+            ],
+            [
+                'course_id' => '26',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '3',
+                'title' => ''
+            ],
+            [
                 'course_id' => '9',
+                'title' => ''
+            ],
+            [
+                'course_id' => '15',
+                'title' => ''
+            ],
+            [
+                'course_id' => '21',
+                'title' => ''
+            ],
+            [
+                'course_id' => '27',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '4',
                 'title' => ''
             ],
             [
@@ -50,11 +84,191 @@ class VideoCategoryNew extends Seeder
                 'title' => ''
             ],
             [
+                'course_id' => '16',
+                'title' => ''
+            ],
+            [
+                'course_id' => '22',
+                'title' => ''
+            ],
+            [
+                'course_id' => '28',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '5',
+                'title' => ''
+            ],
+            [
                 'course_id' => '11',
                 'title' => ''
             ],
             [
+                'course_id' => '17',
+                'title' => ''
+            ],
+            [
+                'course_id' => '23',
+                'title' => ''
+            ],
+            [
+                'course_id' => '29',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '6',
+                'title' => ''
+            ],
+            [
                 'course_id' => '12',
+                'title' => ''
+            ],
+            [
+                'course_id' => '18',
+                'title' => ''
+            ],
+            [
+                'course_id' => '24',
+                'title' => ''
+            ],
+            [
+                'course_id' => '30',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '31',
+                'title' => ''
+            ],
+            [
+                'course_id' => '32',
+                'title' => ''
+            ],
+            [
+                'course_id' => '33',
+                'title' => ''
+            ],
+            [
+                'course_id' => '34',
+                'title' => ''
+            ],
+            [
+                'course_id' => '35',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '36',
+                'title' => ''
+            ],
+            [
+                'course_id' => '37',
+                'title' => ''
+            ],
+            [
+                'course_id' => '38',
+                'title' => ''
+            ],
+            [
+                'course_id' => '39',
+                'title' => ''
+            ],
+            [
+                'course_id' => '40',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '41',
+                'title' => ''
+            ],
+            [
+                'course_id' => '42',
+                'title' => ''
+            ],
+            [
+                'course_id' => '43',
+                'title' => ''
+            ],
+            [
+                'course_id' => '44',
+                'title' => ''
+            ],
+            [
+                'course_id' => '45',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '46',
+                'title' => ''
+            ],
+            [
+                'course_id' => '47',
+                'title' => ''
+            ],
+            [
+                'course_id' => '48',
+                'title' => ''
+            ],
+            [
+                'course_id' => '49',
+                'title' => ''
+            ],
+            [
+                'course_id' => '50',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '51',
+                'title' => ''
+            ],
+            [
+                'course_id' => '52',
+                'title' => ''
+            ],
+            [
+                'course_id' => '53',
+                'title' => ''
+            ],
+            [
+                'course_id' => '54',
+                'title' => ''
+            ],
+            [
+                'course_id' => '55',
+                'title' => ''
+            ],
+
+
+            [
+                'course_id' => '56',
+                'title' => ''
+            ],
+            [
+                'course_id' => '57',
+                'title' => ''
+            ],
+            [
+                'course_id' => '58',
+                'title' => ''
+            ],
+            [
+                'course_id' => '59',
+                'title' => ''
+            ],
+            [
+                'course_id' => '60',
                 'title' => ''
             ],
         ];
