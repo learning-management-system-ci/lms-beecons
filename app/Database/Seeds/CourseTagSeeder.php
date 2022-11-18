3750999e2a55bfce1757146bbc88e7f38cf442b8
vandinta <89644669+vandinta@users.noreply.github.com>
2022-11-18 15:53:23 +0700

    Changes to be committed: modified:   app/Controllers/Api/VideoController.php modified:   app/Database/Seeds/CourseTagSeeder.php

--- app/Database/Seeds/CourseTagSeeder.php
+++ app/Database/Seeds/CourseTagSeeder.php
@@ -9,6 +9,7 @@ class CourseTagSeeder extends Seeder
     public function run()
     {
         $data = [
+            // course 1-6 praduta
             [
                 'course_id' => 1,
                 'tag_id' => 1,
@@ -17,12 +18,10 @@ class CourseTagSeeder extends Seeder
                 'course_id' => 1,
                 'tag_id' => 2,
             ],
-
             [
                 'course_id' => 1,
                 'tag_id' => 3,
             ],
-
             [
                 'course_id' => 2,
                 'tag_id' => 1,
@@ -35,35 +34,32 @@ class CourseTagSeeder extends Seeder
                 'course_id' => 2,
                 'tag_id' => 3,
             ],
-
             [
                 'course_id' => 3,
                 'tag_id' => 1,
             ],
-            [
-                'course_id' => 3,
-                'tag_id' => 2,
-            ],
-            [
-                'course_id' => 3,
-                'tag_id' => 3,
-            ],
-
             [
                 'course_id' => 4,
-                'tag_id' => 1,
-            ],
-
-            [
-                'course_id' => 5,
                 'tag_id' => 2,
             ],
-
+            [
+                'course_id' => 5,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 6,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 6,
+                'tag_id' => 2,
+            ],
             [
                 'course_id' => 6,
                 'tag_id' => 3,
             ],
 
+            // course 1-6 musika
             [
                 'course_id' => 7,
                 'tag_id' => 1,
@@ -76,45 +72,442 @@ class CourseTagSeeder extends Seeder
                 'course_id' => 7,
                 'tag_id' => 3,
             ],
-
             [
                 'course_id' => 8,
                 'tag_id' => 1,
             ],
-
+            [
+                'course_id' => 8,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 8,
+                'tag_id' => 3,
+            ],
             [
                 'course_id' => 9,
-                'tag_id' => 2,
+                'tag_id' => 1,
             ],
-
             [
                 'course_id' => 10,
-                'tag_id' => 3,
-            ],
-
-            [
-                'course_id' => 11,
-                'tag_id' => 1,
-            ],
-            [
-                'course_id' => 11,
                 'tag_id' => 2,
             ],
             [
                 'course_id' => 11,
                 'tag_id' => 3,
             ],
-
             [
                 'course_id' => 12,
                 'tag_id' => 1,
             ],
             [
                 'course_id' => 12,
-                'tag_id' => 2,
+                'tag_id' => 1,
             ],
             [
                 'course_id' => 12,
+                'tag_id' => 3,
+            ],
+
+            // course 1-6 solusi utama
+            [
+                'course_id' => 13,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 13,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 13,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 14,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 14,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 14,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 15,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 16,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 17,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 18,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 18,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 18,
+                'tag_id' => 3,
+            ],
+
+            // course 1-6 madutama
+            [
+                'course_id' => 19,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 19,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 19,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 20,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 20,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 20,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 21,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 22,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 23,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 24,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 24,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 24,
+                'tag_id' => 3,
+            ],
+
+            // course 1-6 multicitra
+            [
+                'course_id' => 25,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 25,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 25,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 26,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 26,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 26,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 27,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 28,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 29,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 30,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 30,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 30,
+                'tag_id' => 3,
+            ],
+
+            // course 7
+            [
+                'course_id' => 31,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 32,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 33,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 34,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 35,
+                'tag_id' => 1,
+            ],
+            
+            // course 8
+            [
+                'course_id' => 36,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 37,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 38,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 39,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 40,
+                'tag_id' => 2,
+            ],
+
+            // course 9
+            [
+                'course_id' => 41,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 42,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 43,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 44,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 45,
+                'tag_id' => 1,
+            ],
+
+            // course 10
+            [
+                'course_id' => 46,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 46,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 46,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 47,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 47,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 47,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 48,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 48,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 48,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 49,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 49,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 49,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 50,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 50,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 50,
+                'tag_id' => 3,
+            ],
+
+            // course 11
+            [
+                'course_id' => 51,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 51,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 51,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 52,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 52,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 52,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 53,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 53,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 53,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 54,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 54,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 54,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 55,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 55,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 55,
+                'tag_id' => 3,
+            ],
+
+            // course 12
+            [
+                'course_id' => 56,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 56,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 56,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 57,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 57,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 57,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 58,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 58,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 58,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 59,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 59,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 59,
+                'tag_id' => 3,
+            ],
+            [
+                'course_id' => 60,
+                'tag_id' => 1,
+            ],
+            [
+                'course_id' => 60,
+                'tag_id' => 2,
+            ],
+            [
+                'course_id' => 60,
                 'tag_id' => 3,
             ],
         ];
