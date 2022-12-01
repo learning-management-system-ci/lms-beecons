58d6712ecf36f3ecd4af491bf152eb9d06907c75
Wahyu Triono <whytrno@gmail.com>
2022-12-01 10:51:40 +0700

    hide sudah baca button when user is not logged in

--- app/Views/components/home/navbar.php
+++ app/Views/components/home/navbar.php
@@ -3,35 +3,34 @@
         <a class="navbar-brand" href="/">
             <img src="../../../image/logo.svg" alt="logo" height="60px" width="60px">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarApp"
-            aria-controls="navbarApp" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarApp" aria-controls="navbarApp" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarApp">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-center">
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == '/') : echo 'active'; endif ?>" aria-current="page"
-                        href="/">Home</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == '/') : echo 'active';
+                                            endif ?>" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == 'courses' || str_contains(uri_string(), 'course')) : echo 'active'; endif ?>"
-                        href="/courses">Courses</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == 'courses' || str_contains(uri_string(), 'course')) : echo 'active';
+                                            endif ?>" href="/courses">Courses</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == 'training') : echo 'active'; endif ?>"
-                        href="/training">Training</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == 'training') : echo 'active';
+                                            endif ?>" href="/training">Training</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == 'webinar') : echo 'active'; endif ?>"
-                        href="/webinar">Webinar</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == 'webinar') : echo 'active';
+                                            endif ?>" href="/webinar">Webinar</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == 'faq') : echo 'active'; endif ?>"
-                        href="/faq">FAQ</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == 'faq') : echo 'active';
+                                            endif ?>" href="/faq">FAQ</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link mx-2 <?php if(uri_string() == 'about-us') : echo 'active'; endif ?>"
-                        href="/about-us">About Us</a>
+                    <a class="nav-link mx-2 <?php if (uri_string() == 'about-us') : echo 'active';
+                                            endif ?>" href="/about-us">About Us</a>
                 </li>
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
@@ -43,8 +42,7 @@
                             </form>
                             <i class="fa-solid fa-xmark" id="nav-btn-search-x"></i>
                         </div>
-                        <button class="nav-btn-icon my-1" id="nav-btn-search" data-bs-toggle="dropdown"
-                            aria-expanded="false" data-bs-auto-close="false">
+                        <button class="nav-btn-icon my-1" id="nav-btn-search" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="false">
                             <i class="fa-solid fa-magnifying-glass"></i>
                         </button>
                         <div class="dropdown-menu my-2" aria-labelledby="nav-btn-search">
@@ -72,82 +70,51 @@
                 </li>
                 <li class="nav-item me-3">
                     <div class="dropdown nav-item-icon">
-                        <button class="nav-btn-icon my-1" id="dropdown-notification" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
+                        <button class="nav-btn-icon my-1" id="dropdown-notification" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <!-- <div class="nav-btn-icon-amount">0</div> -->
                             <i class="fa-solid fa-bell"></i>
                         </button>
                         <div class="dropdown-menu notifications dropdown-menu-end" aria-labelledby="dropdown-notification">
                             <div class="header shadow-sm">
                                 <h3 class="mb-0">Notifikasi</h3>
-                                <a href="" class="notifications-baca">Sudah dibaca</a>
+                                <?php if (get_cookie("access_token")) : ?>
+                                    <a href="" class="notifications-baca">Sudah dibaca</a>
+                                <?php endif ?>
                             </div>
                             <?php if (!get_cookie("access_token")) : ?>
-                            <div class="content">
-                                <h3>Kamu belum daftar</h3>
-                                <p>
-                                    Silakan daftar terlebih dahulu untuk melihat detail keranjang belanja kamu dan
-                                    melakukan transaksi pembelian
-                                </p>
-                                <a href="/login" class="nav-link-btn">
-                                    <button class="my-btn btn-sign-in">Sign in</button>
-                                </a>
-                            </div>
+                                <div class="content">
+                                    <h3>Kamu belum daftar</h3>
+                                    <p>
+                                        Silakan daftar terlebih dahulu untuk melihat detail keranjang belanja kamu dan
+                                        melakukan transaksi pembelian
+                                    </p>
+                                    <a href="/login" class="nav-link-btn">
+                                        <button class="my-btn btn-sign-in">Sign in</button>
+                                    </a>
+                                </div>
                             <?php else : ?>
-                            <div class="notifications-list">
-                                <!-- unread notification -->
-                                <!-- <div class="notif unread">
-                                        <a href="" class="">
-                                            <div class="icon">
-                                                <img src="/image/home/notif-icon.png" alt="icon">
-                                            </div>
-                                            <div class="item">
-                                                <p>
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                                                    quae.
-                                                </p>
-                                                <span>1 jam yang lalu</span>
-                                            </div>
-                                        </a>
-                                    </div> -->
-                                <!-- readed notification -->
-                                <!-- <div class="notif">
-                                        <a href="" class="">
-                                            <div class="icon">
-                                                <img src="/image/home/notif-icon.png" alt="icon">
-                                            </div>
-                                            <div class="item">
-                                                <p>
-                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                                                    quae.
-                                                </p>
-                                                <span>1 jam yang lalu</span>
-                                            </div>
-                                        </a>
-                                    </div> -->
-                            </div>
+                                <div class="notifications-list"></div>
                             <?php endif ?>
                         </div>
                     </div>
                 </li>
                 <?php if (!get_cookie("access_token")) : ?>
-                <li class="nav-item">
-                    <a href="/login" class="nav-link-btn">
-                        <button class="my-btn btn-sign-in">Sign in</button>
-                    </a>
-                </li>
+                    <li class="nav-item">
+                        <a href="/login" class="nav-link-btn">
+                            <button class="my-btn btn-sign-in">Sign in</button>
+                        </a>
+                    </li>
                 <?php else : ?>
-                <li class="nav-item">
-                    <div class="dropdown nav-item-profile">
-                        <button class="nav-btn-profile bg-transparent" id="dropdown-profile" data-bs-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">
-                        </button>
-                        <div class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdown-profile">
-                            <a href="/profile" class="dropdown-item">Profile</a>
-                            <a href="/" class="dropdown-item" id="btn-logout">Logout</a>
+                    <li class="nav-item">
+                        <div class="dropdown nav-item-profile">
+                            <button class="nav-btn-profile bg-transparent" id="dropdown-profile" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-end mt-2" aria-labelledby="dropdown-profile">
+                                <a href="/profile" class="dropdown-item">Profile</a>
+                                <a href="/" class="dropdown-item" id="btn-logout">Logout</a>
+                            </div>
                         </div>
-                    </div>
-                </li>
+                    </li>
                 <?php endif ?>
             </ul>
         </div>
