9ce7fc7787e0ebf9699300e229b5dc8e0d29d42f
rikzan fernanda <fernandarikzan@gmail.com>
2022-12-09 16:40:07 +0700

    change container and update hero

--- app/Views/components/home/hero.php
+++ app/Views/components/home/hero.php
@@ -1,18 +1,20 @@
 <!-- hero -->
 <div id="hero">
     <div class="content-container">
-        <div class="content col-md-6">
-            <h1>
-                The First Group - Mentoring Focused Course in Indonesia.
-            </h1>
-            <p>
-                The learning program in the tender is carried out collaboratively and with the guidance of experienced mentors who make the online learning experience fun and focused.
-            </p>
-            <a href="/about-us">
-                <button class="my-btn">
-                    Pelajari Lebih Lanjut
-                </button>
-            </a>
+        <div class="container">
+            <div class="content col-md-6">
+                <h1>
+                    The First Group - Mentoring Focused Course in Indonesia.
+                </h1>
+                <p>
+                    Tender merupakan tempat dimana kamu dapat mempelajari suatu bidang baru yang akan langsung dibimbing oleh para mentor ahli di bidangnya.
+                </p>
+                <a href="/about-us">
+                    <button class="app-btn">
+                        Pelajari Lebih Lanjut
+                    </button>
+                </a>
+            </div>
         </div>
     </div>
     <div class="image d-none d-md-block">
