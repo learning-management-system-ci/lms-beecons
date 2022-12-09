dcf507a337f0bc69ae9af8e7a9ba721e31cf175a
salunaalavi <salunaalavi@gmail.com>
2022-12-09 11:03:35 +0700

    Adding consume pap and re-validate edit profile

--- app/Views/pages/navigation/terms_and_condition.php
+++ app/Views/pages/navigation/terms_and_condition.php
@@ -14,15 +14,7 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nesciunt maxime laboriosam minus iste fuga
                 odit repellat delectus iusto. Temporibus, praesentium dicta. Quae earum natus molestias ex neque
                 eveniet dicta?</p>
-            <section>
-                <H3>1. Abc Lima Dasar</H3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nesciunt maxime laboriosam minus iste fuga
-                    odit repellat delectus iusto. Temporibus, praesentium dicta. Quae earum natus molestias ex neque
-                    eveniet dicta?</p>
-                <H3>2. Abc Lima Dasar</H3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A nesciunt maxime laboriosam minus iste fuga
-                    odit repellat delectus iusto. Temporibus, praesentium dicta. Quae earum natus molestias ex neque
-                    eveniet dicta?</p>
+            <section class="data">
             </section>
         </div>
     </section>
@@ -30,5 +22,5 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('js-component') ?>
-<script src="js/home/faq.js"></script>
+<script src="../../../js/pap/index.js"></script>
 <?= $this->endSection() ?>
