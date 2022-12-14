33b71e644e030ba324b4e7184b288de5a915ffc4
Fakhri112 <dracozen112@gmail.com>
2022-12-14 18:08:03 +0700

    integrating course video api in admin, fix and improve several UI

--- public/js/api/admin/videoDetail.js
+++ public/js/api/admin/videoDetail.js
@@ -1,8 +1,17 @@
 // check the cookies if the user is logged in
-if(!Cookies.get('access_token')){
+if (!Cookies.get('access_token')) {
   // if not, redirect to login page
   window.location.href = '/login'
 }
+
+const content_edit = {
+  id: $('#video-id-input'),
+  cat_id: $('#video-id-category'),
+  title: $('#title-input'),
+  thumbnail: $('#thumbnail-input'),
+  video: $('#video-input'),
+  order: $('#video-order'),
+};
 
 const startEdit = () => {
   const editPage = $('#edit-page')
@@ -35,19 +44,16 @@ $(document).ready(() => {
     }
     let data
     await $.ajax(option)
+    console.log(data)
     return data
   }
 
-  const populateEditVideoData = ( data ) => {
-    const content = {
-      id: $('#video-id-input'),
-      title: $('#title-input'),
-      thumbnail: $('#thumbnail-input'),
-      video: $('#video-input'),
-    };
+  const populateEditVideoData = (data) => {
 
-    content.id.val(data.video_id)
-    content.title.val(data.title)
+    content_edit.id.val(data.video_id)
+    content_edit.title.val(data.title)
+    content_edit.cat_id.val(data.video_category_id)
+    content_edit.order.val(data.order)
 
   }
 
@@ -66,13 +72,72 @@ $(document).ready(() => {
 
     content.id.text(video.video_id)
     content.title.text(video.title)
-    content.thumbnail.attr('src', video.thumbnail)
-    console.log(video)
-    content.video_wraper.empty()
-    content.video_wraper.innerHTML = `
-    <source class="video-content" src="${video.video}" type="video/mp4">
-    Your browser does not support the video tag.
-    `
+    content.thumbnail.replaceWith(`<img class="img-thumbnail" width="400px" alt="thumbanail" src='${video.thumbnail}'>`)
+    // content.video_wraper.empty()
+    // content.video_wraper.innerHTML = `
+    // <source class="video-content" src="${video.video}" type="video/mp4">
+    // Your browser does not support the video tag.
+    // `
+    content.video_wraper.attr('src', video.video)
+
+
+
+    $('#video-edit-form').on('submit', function (e) {
+      e.preventDefault()
+      let form = new FormData($('#video-edit-form')[0])
+
+      $.ajax({
+        type: "POST",
+        url: `/api/course/video/update/${content_edit.id.val()}`,
+        data: form,
+        dataType: 'json',
+        contentType: false,
+        cache: false,
+        processData: false,
+        headers: {
+          "Authorization": `Bearer ${Cookies.get("access_token")}`,
+        },
+        beforeSend: function () {
+          Swal.fire({
+            width: '300px',
+            title: "<div class='status-loading'> " +
+              '<img class="loading-icon" src="image/cart/redeem-loading.gif" alt=""> ' +
+              '<p>Sedang Mengirim...(<span id="upload-loading"></span>)</p> ' +
+              "</div>",
+            padding: '0px 0px 40px 6px',
+            showConfirmButton: false,
+            showClass: {
+              popup: 'animate__animated animate__fadeIn animate__fast'
+            },
+          })
+        },
+        xhr: function () {
+          var xhr = new window.XMLHttpRequest();
+          xhr.upload.addEventListener("progress", function (evt) {
+            if (evt.lengthComputable) {
+              var percentComplete = evt.loaded / evt.total;
+              console.log(percentComplete);
+              $('#upload-loading').html((Math.round(percentComplete * 100)) + '%');
+            }
+          }, false);
+          return xhr;
+        },
+        success: function () {
+          return Swal.fire({
+            width: '300px',
+            title: "<div class='status-loading'> " +
+              '<h5>Success</h5> ' +
+              "</div>",
+            showConfirmButton: true,
+            showClass: {
+              popup: 'animate__animated animate__fadeIn animate__fast'
+            },
+          }).then(function () {
+            window.location.reload()
+          });
+        },
+      })
+    })
   }
 
 
