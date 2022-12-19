e644e46a830c7718dcfe65f03969202ca60509e1
rikzan fernanda <fernandarikzan@gmail.com>
2022-12-19 13:13:32 +0700

    integrate training

--- public/js/home/homepage.js
+++ public/js/home/homepage.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
     // handle courses
     handleCourses()
 
+    // handle training
+    handleTraining()
+
     // handle webinar
-    handleWebinar()
+    // handleWebinar()
 
     // handle mentor slider
     handleMentor()
@@ -190,6 +193,51 @@ async function handleCourses() {
                 })
             })
         }
+    } catch (error) {
+        // console.log(error)
+    }
+}
+
+async function handleTraining() {
+    try {
+        const trainingResponse = await $.ajax({
+            url: '/api/course/filter/training/3',
+            method: 'GET',
+            dataType: 'json'
+        })
+
+        console.log(trainingResponse)
+        $('#training .training-wrapper').html(trainingResponse.map(training => {
+            return `
+                <div class="col-md-4">
+                    <div class="card-training">
+                        <div class="thumbnail">
+                            <img src="${training.thumbnail}" alt="thumbnail">
+                        </div>
+                        <div class="title">
+                            <h2 class="text-truncate">${training.title}</h2>
+                        </div>
+                        <div class="body">
+                            <p class="mb-2">
+                                ${textTruncate(training.description, 130)}
+                            </p>
+                            <div class="info d-flex align-items-center gap-2">
+                                <i class="fa-solid fa-house"></i>   
+                                <p class="m-0">In House Traning</p>
+                            </div>
+                        </div>
+                        <div class="price my-1 mb-2">
+                            <p class="m-0">${getRupiah(training.new_price)}</p>
+                        </div>
+                        <div class="btn-wrapper">
+                            <a href="${`/training/${training.course_id}`}">
+                                <button class="app-btn btn-full">Lihat Detail</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            `
+        }))
     } catch (error) {
         // console.log(error)
     }
