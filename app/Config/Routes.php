bf6d23296ecca5ab4838ef563dffdc8ac99eb061
hendrikusriko <88796037+hendrikusriko@users.noreply.github.com>
2022-12-20 19:02:54 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -321,7 +321,7 @@ $routes->group('api/', static function ($routes) {
     $routes->group('order/', static function ($routes) {
         $routes->get('', 'Api\OrderController::index');
         $routes->get('generatesnap', 'Api\OrderController::generateSnap');
-        $routes->get('notif-handler', 'Api\OrderController::notifHandler');
+        $routes->post('notif-handler', 'Api\OrderController::notifHandler');
 
         $routes->post('coba', 'Api\OrderController::send');
     });
