40ae6f206e2fc5e5d5b0d5c22e5e36ef89200251
rikzan fernanda <fernandarikzan@gmail.com>
2022-12-21 11:13:19 +0700

    update course list layout

--- app/Views/pages/navigation/courses.php
+++ app/Views/pages/navigation/courses.php
@@ -9,10 +9,88 @@
 <?= $this->section('app-component') ?>
 
 <div id="courses">
-
-    <div class=" courses-list pt-4">
+    <div class="courses-list pt-4">
         <div class="container">
-            <div class="bg-white rounded courses-list border">
+            <div class="pb-5">
+                <div class="text-end mb-4">
+                    <span class="btn-filter-wrapper">
+                        <button class="app-btn btn-filter">
+                            <span>Filters</span>
+                            <i class="fa-solid fa-filter"></i>
+                        </button>
+                        <div class="filter-container d-none">
+                            <div class="filter-container__header">
+                                <h3>Filters</h3>
+                                <a href="">Clear all</a>
+                            </div>
+                            <div class="accordion">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-bidang">
+                                            Bidang
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-bidang" class="accordion-collapse p-2 collapse show">
+                                        <div class="filter-item text-start">
+                                            <div class="d-flex gap-2">
+                                                <input type="checkbox" name="bidang" value="engineering">
+                                                <label>Engineering</label>
+                                            </div>
+                                            <div class="d-flex gap-2">
+                                                <input type="checkbox" name="bidang" value="it">
+                                                <label>IT</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-kelas">
+                                            Kelas
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-kelas" class="accordion-collapse p-2 collapse">
+                                        <div class="filter-item text-start"></div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#accordion-tingkat">
+                                            Tingkat
+                                        </button>
+                                    </h2>
+                                    <div id="accordion-tingkat" class="accordion-collapse p-2 collapse">
+                                        <div class="filter-item text-start"></div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button class="app-btn btn-full mt-3">Apply</button>
+                        </div>
+                    </span>
+                </div>
+
+                <div id="courses-loading">
+                    <div class="stage">
+                        <div class="dot-pulse">
+                        </div>
+                    </div>
+                </div>
+
+                <div id="courses-list" class="row"></div>
+
+                <!-- PAGINATION -->
+                <div class="courses-pagination mt-5">
+                    <div class="btn-pgn-prev-wrapper"></div>
+                    <div class="btn-pgn-wrapper"></div>
+                    <div class="btn-pgn-next-wrapper"></div>
+                </div>
+                <!-- END PAGINATION -->
+            </div>
+
+
+
+            <div class="bg-white rounded courses-list border d-none">
                 <ul class="nav nav-tabs nav-fill mb-3 bg-gray" role="tablist">
                     <li class="nav-item" role="presentation">
                         <a class="nav-link active" data-bs-toggle="tab" href="#tab-courses-engineering" role="tab" aria-controls="tab-courses-1" aria-selected="true">Engineering</a>
@@ -66,7 +144,11 @@
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
+    <div>
+        <div class="container">
             <div class="courses-bundling">
                 <div class="my-container">
                     <h1 class="">Ikuti beberapa kursus dengan pilihan paket bundling!</h1>
@@ -81,11 +163,6 @@
                     <div class="row justify-content-md-center gap-4 py-3 courses-bundling-rekomendasi"></div>
                 </div>
             </div>
-        </div>
-    </div>
-
-    <div>
-        <div class="container">
             <nav class="mt-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item active" aria-current="page">Bundling</li>
@@ -116,6 +193,6 @@
 <?= $this->section('js-component') ?>
 <script src="../../../js/utils/getRupiah.js"></script>
 <script src="../../../js/utils/textTruncate.js"></script>
-<script src="../../../js/home/courses.js"></script>
+<script src="../../../js/home/courses2.js"></script>
 <script src="../../../js/home/courses-bundling.js"></script>
 <?= $this->endSection() ?>
